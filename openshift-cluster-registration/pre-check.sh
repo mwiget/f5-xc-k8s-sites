66b7f8f2a7dd2d4da8159d7e385e6d51610e03bc
Marcel Wiget <m.wiget@f5.com>
2022-06-20 14:31:48 +0000

    openshift cluster registered

--- openshift-cluster-registration/pre-check.sh
+++ openshift-cluster-registration/pre-check.sh
@@ -2,7 +2,7 @@
 cd ../openshift-cluster
 CLUSTER_NAME=$(oc status|grep https://api|cut -d/ -f3|cut -d. -f2)
 while [ $(kubectl --kubeconfig $CLUSTER_NAME/auth/kubeconfig get pods -n ves-system|grep vp-manager-|grep Running|wc -l) -ne 3 ] ; do
-  echo "waiting for gke-cluster ($SECONDS secs) ..."
+  echo "waiting for openshift-cluster ($SECONDS secs) ..."
   sleep 1
 done
 echo "openshift-cluster ready"
